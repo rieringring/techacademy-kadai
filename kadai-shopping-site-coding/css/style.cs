4a9eb9af40b7092c6da4d27428ca6aa296ada94c
EC2 Default User <ec2-user@ip-172-31-55-133.ec2.internal>
2020-12-09 05:06:41 +0000

    kadai-shopping-site-coding

---
+++ kadai-shopping-site-coding/css/style.cs
@@ -0,0 +1,85 @@
+@charset "UTF-8";
+@import url("https://fonts.googleapis.com/css?family=Abel");
+/* Base
+------------------------------------------------------------*/
+body {
+  max-width: 940px;
+  margin: 0 auto;
+  background-color: #F9F8F6;
+  font-size: 1em;
+  font-family: "游ゴシック", YuGothic, "メイリオ", Verdana, "Hiragino Kaku Gothic ProN", Meiryo, sans-serif;
+  line-height: 1;
+  letter-spacing: 0.1em; }
+
+p {
+  padding: 10px 10px 10px 0px;
+  font-size: 0.8em; }
+
+h3 {
+  padding: 10px 10px 10px 0px;
+  color: #887f69; }
+
+a {
+  text-decoration: none;
+  color: black; }
+
+header {
+  display: flex;
+  justify-content: space-between;
+  padding: 30px; }
+  header nav {
+    padding: 20px; }
+    header nav ul {
+      display: flex; }
+      header nav ul li {
+        padding: 10px; }
+
+main {
+  display: flex;
+  justify-content: space-between;
+  max-width: 940px;
+  margin: 0 auto; }
+  main .container {
+    width: 74.4%;
+    padding: 20px 0px; }
+  main .container h2 {
+    padding: 40px; }
+  main .list-item-box {
+    width: 33%; }
+  main .list-item {
+    flex-wrap: wrap;
+    display: flex; }
+  main .sidebar {
+    padding: 20px; }
+    main .sidebar .sige-search {
+      height: 80px;
+      background-color: white; }
+      main .sidebar .sige-search .side-search-logo {
+        padding: 10px 30px 30px 20px; }
+  main .side-categories {
+    background: white; }
+    main .side-categories .side-category-link {
+      display: inline-block;
+      padding: 20px 0px; }
+  main .side-logo {
+    background: #887f69;
+    padding: 20px 40px 30px 20px;
+    margin: 15px 15px 10px 0px; }
+  main .shoppng-guide-inner {
+    background-color: white;
+    display: flex; }
+    main .shoppng-guide-inner .shoppng-guide-item {
+      padding: 20px 30px; }
+
+footer {
+  background-color: yellow;
+  max-width: 940px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between; }
+  footer .footer-contents {
+    display: flex; }
+  footer .pagetop-logo {
+    justify-content: space-between; }
+
+/*# sourceMappingURL=style.cs.map */
